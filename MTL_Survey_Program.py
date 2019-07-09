42d248a695aed9dc423b4745af61e9ba87ee8dfb
philip.griffith <philip.griffith@gmail.com>
2019-07-09 12:05:29 +0300

    Added Point class and refactored preexisting code.

--- MTL_Survey_Program.py
+++ MTL_Survey_Program.py
@@ -24,10 +24,101 @@ class Utils:
         else:
             return fnew
 
+    @staticmethod
+    def _valid_ftype(f):
+        valid_ftypes = ('.dxf', '.pnt', '.csv')
+        ext = os.path.splitext(f)[1]
+        return ext if ext in valid_ftypes else None
 
-class DXF(Utils):
+
+class Data(Utils):
 
     def __init__(self, points=None):
+        self.points = []
+        if points:
+            self.add(points)
+
+    def add(self, data):
+        if os.path.isdir(data):
+            for root, dirs, fs in os.walk(data):
+                for f in fs:
+                    ftype = self._valid_ftype(f)
+                    if ftype:
+                        fpath = os.path.join(root, f)
+                        pnt = self._get_data(fpath, ftype)
+                        self.points.append(pnt)
+                        print('{} added'.format(fpath))
+        elif os.path.isfile(data):
+            ftype = self._valid_ftype(data)
+            if ftype:
+                pnt = self._get_data(data, ftype)
+                self.points.append(pnt)
+                print('{} added'.format(data))
+        return
+
+    def _get_data(self, fpath, ftype):
+
+        with open(fpath) as f:
+            if ftype == '.pnt':
+                data = []
+                for line in f:
+                    pnt = self.get_data_topcon(line)
+                    data.append(pnt)
+            elif ftype == '.dxf':
+                lines = f.readlines()
+                data = self._get_data_dxf(lines)
+            elif ftype == '.csv':
+                data = []
+                for line in f:
+                    pnt = self._get_data_nikon(line)
+                    data.append(pnt)
+        return data
+
+    @staticmethod
+    def _get_data_topcon(line):
+
+        return line.rstrip().rstrip(',')
+
+    def _get_data_dxf(self, lines):
+
+        data = []
+        for i, line in enumerate(lines):
+            if i > 164:
+                try:
+                    line = line.strip()
+                    if 'POINTNAMEBLOCK' in line:
+                        pnt_line = self._get_coords(lines[i:i + 59])
+                        if pnt_line[0] == 'ACDBENTITY':
+                            pnt_line[0] = lines[i + 108].strip().upper()
+                        line = ','.join(str(i) for i in pnt_line)
+                        data.append(line)
+                except ValueError:
+                    pass
+        return data
+
+    @staticmethod
+    def _get_data_nikon(line):
+
+        data = line.rstrip().split(',')[1:]
+        n = data[0]
+        e = data[1]
+        z = data[2]
+        desc = data[3]
+        return '{},{},{},{}'.format(desc, e, n, z)
+
+    @staticmethod
+    def _get_coords(content):
+
+        desc = content[54].strip().upper()
+        e = float(content[2])
+        n = float(content[4])
+        z = float(content[6])
+        return [desc, e, n, z]
+
+
+class DXF():
+
+    def __init__(self):
         self.start = "0\nSECTION\n2\nHEADER\n 9\n$ACADVER\n 1\nAC1009\n 9\n$EXTMIN\n 10\n" \
             "0.00000000\n 20\n0.00000000\n 30\n0.00000000\n 9\n$EXTMAX\n 10\n" \
             "0.00000000\n 20\n0.00000000\n 30\n0.00000000\n 9\n$LIMMIN\n 10\n" \
@@ -46,30 +137,6 @@ class DXF(Utils):
             "DESCRIPTION\n 1\n\n 10\n0.00000000\n 20\n-0.04000000\n 30\n0.00000000\n" \
             "0\nENDBLK\n0\nENDSEC\n0\nSECTION\n2\nENTITIES\n"
         self.end = "0\nENDSEC\n0\nEOF\n"
-        self.points = self.add(points)
-
-    def add(self, data):
-        payload = []
-        if os.path.isdir(data):
-            for root, dirs, fs in os.walk(data):
-                for f in fs:
-                    if self.check_ftype(f):
-                        pnt = self.make_point(os.path.join(root, f))
-                        payload.append(pnt)
-        elif os.path.isfile(data):
-            if self.check_ftype(data):
-                pnt = self.make_point(data)
-                payload.append(pnt)
-        print(payload)
-        return payload
-
-    def check_ftype(self, f):
-        ext = os.path.splitext(f)[1]
-        return True if ext in ('.dxf', '.pnt') else False
-
-    def make_point(self, data):
-        pass
-        return
 
     @staticmethod
     def _make_pointnameblock(num, code, x, y, z):
@@ -134,70 +201,6 @@ class DXF(Utils):
         return
 
 
-def get_coords(content):
-
-    desc = content[54].strip().upper()
-    e = float(content[2])
-    n = float(content[4])
-    z = float(content[6])
-
-    return [desc, e, n, z]
-
-
-def get_points_nikon(line):
-
-    data = line.rstrip().split(',')[1:]
-    n = data[0]
-    e = data[1]
-    z = data[2]
-    desc = data[3]
-
-    return '{},{},{},{}'.format(desc, e, n, z)
-
-
-def get_points_topcon(line):
-
-    return line.rstrip().rstrip(',')
-
-
-def get_points_dxf(lines):
-
-    points = []
-    for i, line in enumerate(lines):
-        if i > 164:
-            try:
-                line = line.strip()
-                if 'POINTNAMEBLOCK' in line:
-                    pnt_line = get_coords(lines[i:i+59])
-                    if pnt_line[0] == 'ACDBENTITY':
-                        pnt_line[0] = lines[i+108].strip().upper()
-                    line = ','.join(str(i) for i in pnt_line)
-                    points.append(line)
-            except ValueError:
-                pass
-                
-    return points
-
-
-def get_points(path, ts):
-    
-    points = []
-    with open(path) as f:
-        if ts.upper() == 'NIKON':
-            for line in f:
-                pnt_line = get_points_nikon(line)
-                points.append(pnt_line)
-        elif ts.upper() == 'TOPCON':
-            for line in f:
-                pnt_line = get_points_topcon(line)
-                points.append(pnt_line)
-        elif ts.upper() == 'DXF':
-            lines = f.readlines()
-            points = get_points_dxf(lines)
-
-    return points
-
-
 def make_pnt(fname, points):
 
     pnt_fname = format_fname(fname, 'pnt')    
@@ -210,24 +213,24 @@ def make_pnt(fname, points):
     return pnt_fname
 
 
-def processor(fname):
-
-    points = []
-    ftypes = ['.csv', '.pnt', '.dxf']
-    fl = fname.lower()
-    if os.path.splitext(fl)[1] in ftypes:
-        path = os.path.join('./', fname)
-        try:
-            if fl.endswith('.csv'):
-                points = get_points(path, 'Nikon')
-            elif fl.endswith('.pnt'):
-                points = get_points(path, 'TopCon')
-            else:
-                points = get_points(path, 'DXF')
-        except IOError:
-            return None
-
-    return points
+# def processor(fname):
+#
+#     points = []
+#     ftypes = ['.csv', '.pnt', '.dxf']
+#     fl = fname.lower()
+#     if os.path.splitext(fl)[1] in ftypes:
+#         path = os.path.join('./', fname)
+#         try:
+#             if fl.endswith('.csv'):
+#                 points = get_points(path, 'Nikon')
+#             elif fl.endswith('.pnt'):
+#                 points = get_points(path, 'TopCon')
+#             else:
+#                 points = get_points(path, 'DXF')
+#         except IOError:
+#             return None
+#
+#     return points
 
 
 def convert_to_dxf(fname):
@@ -524,4 +527,6 @@ if __name__ == '__main__':
     path = 'TEST'
     d = os.path.join(path, 'DIR')
     f = os.path.join(path, '07-19-18.dxf')
-    dxf = DXF(f)
+    dxf = Data(f)
+
+    print(dxf.points)
